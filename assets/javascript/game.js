407d1bd03d2d3e36d1bf52baf90b02aeaec338f9
Jaybird <jrmiller022@gmail.com>
2018-12-08 12:52:59 -0600

    unit 4 game

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+//variables
+var randomNumber = "";
+var wins = 0;
+var losses = 0;
+var counter = 0;
+var images = ["assets/images/blue%20crystal.png","assets/images/green%20crystal.jpeg","assets/images/red%20crystal.ico","assets/images/yellow%20crystal.png"];
+
+//display random number in number generater
+
+
+//when user clicks on image, the stored number will display
+
+
+
+//score will display and add in the score area
+
+
+
+//when user matches the random number the user wins
+
+
+
+//wehn the user goes over the random number, user loses. the game reserts and a new random number is displayed
