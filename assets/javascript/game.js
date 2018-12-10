d2fbebdeaf6abb1ab3d53f154835919d5da1bb63
Jaybird <jrmiller022@gmail.com>
2018-12-09 20:49:27 -0600

    unit 4 game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,55 +2,110 @@
 var randomNumber = 0;
 var wins = 0;
 var losses = 0;
-var images = ["assets/images/blue%20crystal.png","assets/images/green%20crystal.jpeg","assets/images/red%20crystal.ico","assets/images/yellow%20crystal.png"];
 
 //display random number in number generater function
 function number(min, max) {
     randomNumber = Math.floor(Math.random() * ((max - min) + 1) + min);
     return randomNumber;
-}
+};
 
 // Calling function called number to append random number
 $(".random-number").append(number(120, 19));
 
+function reset(){
+    num1= Math.floor(Math.random()*11+1);
+    num2= Math.floor(Math.random()*11+1);
+    num3= Math.floor(Math.random()*11+1);
+    num4= Math.floor(Math.random()*11+1);
+    userTotal= 0;
+    $('#guesses-remaining').append(number(11, 1));
+};
+
 //when user clicks on image, the stored number will display
 var counter = 0;
-$(".crystal-image").on("click", function() {
-    counter = counter + 1;
-    alert("new score: " + counter);
-    $("#guessesremaing").text(userScore);
+$("#one").on("click", function() {
+    var firstCrystal = number(10, 1);
+    counter = counter + firstCrystal;
+    $("#one").append(firstCrystal);
+    alert("Your total score: " + counter);
+    $("#scoreboard").text(scoreboard);
 
     if (counter === randomNumber) {
         alert("You Win!");
     }
-    else if(counter >= randomNumber) {
+    else if(counter > randomNumber) {
         alert("You lose!");
     }
 });
 
+$("#two").on("click", function() {
+    var secondCrystal = number(10, 1);
+    counter = counter + secondCrystal;
+    $("#one").append(secondCrystal);
+    alert("Your total score: " + counter);
+    $("#scoreboard").text(scoreboard);
 
+    if (counter === randomNumber) {
+        alert("You Win!");
+    }
+    else if(counter > randomNumber) {
+        alert("You lose!");
+    }
+});
 
-var numberOptions = [5, 6, 7, 8, 9, 10, 11, 12,]
+$("#three").on("click", function() {
+    var thirdCrystal = number(10, 1);
+    counter = counter + thirdCrystal;
+    $("#one").append(thirdCrystal);
+    alert("Your total score: " + counter);
+    $("#scoreboard").text(scoreboard);
+
+    if (counter === randomNumber) {
+        alert("You Win!");
+    }
+    else if(counter > randomNumber) {
+        alert("You lose!");
+    }
+});
+
+$("#four").on("click", function() {
+    var fourthCrystal = number(10, 1);
+    counter = counter + fourthCrystal;
+    $("#one").append(fourthCrystal);
+    alert("Your total score: " + counter);
+    $("#scoreboard").text(scoreboard);
+
+    if (counter === randomNumber) {
+        alert("You Win!");
+    }
+    else if(counter > randomNumber) {
+        alert("You lose!");
+    }
+});
+
+var numberOptions = [5, 6, 7, 8, 9, 10, 11, 12,];
  
-// Create a for loop to create crystals for every numberOption.
-for (var i = 0; i < numberOption.length; i++) {
-    var crystalimage = $("<img>");
-    crystalimage.addClass("crystal-image");
+// Create a for loop to create crystals for every numberOptions.
+for (var i = 0; i < numberOptions.length; i++) {
+    var crystalImage = $("<img>");
+    crystalImage.addClass("crystal-image");
 
 // Each crystal will be given a src link to the crystal image.
-crystalimage.attr("src", "assets/images/blue%20crystal.png", "assets/images/green%20crystal.jpeg", "assets/images/red%20crystal.ico", "assets/images/yellow%20crystal.png");
+crystalImage.attr("src", "assets/images/blue%20crystal.png", "assets/images/green%20crystal.jpeg", "assets/images/red%20crystal.ico", "assets/images/yellow%20crystal.png");
 
-// Each image crystal will be given a data attribut called data-crystalValue.
+// Each image crystal will be given a data attribute called data-crystalValue.
 // This data will be set equal to the array value.
-crystalimage.attr("data-crystalValue", numberOptions[i]);
+crystalImage.attr("data-crystalValue", numberOptions[i]);
+};
 
 // Each crystal-image with classes/Attributes will be added to the page.
-$("#crystals").append(crystalimage);
+
+
 
 // This will apply to all 4 images all the page.
 
 
-}
+
     
 //score will display and add in the score area
 
